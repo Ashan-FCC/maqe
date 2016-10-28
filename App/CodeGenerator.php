66c8581ca79a38c87c9bf31cf48bb5288561bdfc
Ashan <ashan@ibaht.com>
2016-10-28 22:25:16 +0700

    Complete assignment.
    Add tests.

---
+++ App/CodeGenerator.php
@@ -0,0 +1,69 @@
+<?php
+namespace App;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+class CodeGenerator {
+    private $tokenLength;
+    private $noOfCodes;
+    private $prefix;
+    public $file;
+
+    public function __construct($tokenLength, $noOfCodes, $prefix="MY", $file=""){
+        $this->tokenLength= $tokenLength;
+        $this->noOfCodes = $noOfCodes;
+        $this->prefix  = $prefix;
+        $this->file  = $file;
+    }
+
+    public function generateTokens(){
+    $couponFile = $this->file === "" ? __DIR__."\\..\\storage\\section2.txt" : $this->file;
+    $fp = fopen($couponFile,"wb");
+    $tokenLength = $this->tokenLength - strlen($this->prefix);
+        if($fp!== false){
+            for($i =0 ; $i< $this->noOfCodes; $i++){
+
+            $code = $this->prefix . $this->getToken($tokenLength);
+            fwrite($fp, $code."\n");
+            
+            }
+            fclose($fp);
+        }
+
+    }
+
+    public function getToken($length)
+    {
+        $token = "";
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet.= "0123456789";
+        $max = strlen($codeAlphabet); // edited
+
+        for ($i=0; $i < $length; $i++) {
+            $token .= $codeAlphabet[$this->crypto_rand_secure(0, $max)];
+        }
+
+        return $token;
+    }
+    private function crypto_rand_secure($min, $max)
+    {
+        $range = $max - $min;
+        if ($range < 1) return $min; // not so random...
+        $log = ceil(log($range, 2));
+        $bytes = (int) ($log / 8) + 1; // length in bytes
+        $bits = (int) $log + 1; // length in bits
+        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+        do {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+            $rnd = $rnd & $filter; // discard irrelevant bits
+        } while ($rnd >= $range);
+        return $min + $rnd;
+    }
+
+
+}
+
+
+?>
---
+++ tests/CodeGeneratorTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\CodeGenerator;
+
+class CodeGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    private $generator;
+    public function setUp(){
+        $this->generator = new CodeGenerator(10, 250, $prefix="MY", $file="");
+    }
+    /** @test */
+    public function create_code_with_10_characters()
+    {
+        
+        $token = $this->generator->getToken(10);
+
+        $this->assertTrue(strlen($token) == 10);
+    }
+    /** @test */
+    public function create_all_codes_at_default_location(){
+
+        $this->generator->generateTokens();
+
+        $file = __DIR__.'/../storage/section2.txt';
+        $this->assertTrue(file_exists($file));
+
+        $codes = fopen($file, "r");
+        $count = 0;
+        while(!feof($codes)){
+            $line = fgets($codes);
+            if($line !== false)
+            $count++;
+
+
+        }
+        fclose($codes);
+        $this->assertEquals(250 , $count);
+
+
+
+    }
+    private function removeLineBreaks($line){
+        str_replace("", "", $line);
+        str_replace("\n", "", $line);
+        str_replace("\r\n", "", $line);
+        return $line;
+    }
+
+    // public static function tearDownAfterClass(){
+    //     try{
+    //         $file = __DIR__.'/../storage/section2.txt';
+    //         if(file_exists($file))
+    //         unlink($file);
+    //     }catch(Exception $ex){
+
+    //         echo $ex->getMessage();
+
+    //     }
+        
+
+    // }
+}
+?>
